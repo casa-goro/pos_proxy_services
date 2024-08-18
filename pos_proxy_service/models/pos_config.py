de51f9bfb0202c94ff7662aee3fd4d9b8435a6dc
Filoquin <filoquin@gmail.com>
2024-08-17 22:38:39 -0300

    [FIX] Pos config

--- pos_proxy_service/models/pos_config.py
+++ pos_proxy_service/models/pos_config.py
@@ -9,6 +9,7 @@ class PosConfig(models.Model):
 	use_fiscal_printer = fields.Boolean('Impresora Fiscal')
 	proxy_fiscal_printer = fields.Char('Ip Impresora Fiscal', default='http://127.0.0.1:5005')
 	version_printer = fields.Selection([
+		('', 'Generica'),
 		('hasar250', 'Hasar 250'),
 		('epsont900fa', 'Epson T900FA'),
 	], default='epsont900fa')
